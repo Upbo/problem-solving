4c44025d7d4a34f5f5d2f36dbe89e3afb359bd60
Upbo <khd1326@naver.com>
2019-06-26 20:56:14 +0900

    Update 2845.cpp

--- 2845.cpp
+++ 2845.cpp
@@ -1,13 +1,14 @@
+// https://www.acmicpc.net/problem/2845
 #include <cstdio>
 
 int main()
 {
 	int l, p, expect[5], count;
 	scanf("%d %d", &l, &p);
-    count=l*p;
+   count=l*p;
 	for(int i=0; i<5; i++) 
-    {
+   {
         scanf("%d", &expect[i]);
         printf("%d ", expect[i]-count);
-    }
+   }
 }
