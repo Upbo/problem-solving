2af13cb49e5a10a4f11a17348b05ae408e63a8ff
Upbo <khd1326@naver.com>
2019-06-26 21:05:08 +0900

    Update 2845.cpp

--- 2845.cpp
+++ 2845.cpp
@@ -3,12 +3,13 @@
 
 int main()
 {
-	int l, p, expect[5], count;
-	scanf("%d %d", &l, &p);
-   count=l*p;
-	for(int i=0; i<5; i++) 
-   {
+    int l, p, expect[5], count;
+    scanf("%d %d", &l, &p);
+    count=l*p;
+    for(int i=0; i<5; i++) 
+    {
         scanf("%d", &expect[i]);
         printf("%d ", expect[i]-count);
-   }
+    }
+    return 0; 
 }
