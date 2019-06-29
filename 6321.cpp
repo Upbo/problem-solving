6c01b629ac631413e62f3b9655a226b0fb1ff593
Upbo <khd1326@naver.com>
2019-06-29 16:25:38 +0900

    Solve 6321.cpp

---
+++ 6321.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+
+int main()
+{
+    int n;
+    char tmp[51];
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+    {
+        scanf("%s", tmp);
+        std::string s(tmp);
+        printf("String #%d\n", i);
+        for(int j=0; j<s.size(); j++)
+        {
+            printf("%c", (s[j]-'A'+1)%26+'A';
+        }
+        printf("\n\n");
+        
+    }
+    return 0;
+}
