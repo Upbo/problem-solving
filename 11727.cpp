207d9531fb4dde0b59e7bc91a8843f545af035ec
Upbo <khd1326@naver.com>
2019-07-03 21:07:12 +0900

    Solve 11727.cpp

---
+++ 11727.cpp
@@ -0,0 +1,17 @@
+/* https://www.acmicpc.net/problem/11727 */
+#include <cstdio> 
+long dp[1001];
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=3;
+    for(int i=3; i<=n; i++)
+    {
+        dp[i]=(dp[i-1]+dp[i-2]*2)%10007;
+    }
+    printf("%ld", dp[n]%10007);
+    return 0;
+}
